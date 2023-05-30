d92a341570ae9295d7937585fde8f9b960725372
Alexandr-Well <73768672+Alexandr-Well@users.noreply.github.com>
2023-05-30 13:15:24 +0700

    Update settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -27,7 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if int(os.getenv("DEBUG")):
+    DEBUG = True
+else:
+    DEBUG = False
 
 INTERNAL_IPS = [
     '127.0.0.1',
